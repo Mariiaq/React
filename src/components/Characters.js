a56f994486f6f6a82dda66e325ea3165ee7e0f83
Mariiaq <masha_print@ukr.net>
2022-06-21 16:47:55 +0300

    homework-react-lesson-1

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,17 +1,17 @@
-// function Characters(props) {
-//     let {name, desk, foto} = props;
-//
-//     return (
-//         <div>
-//             <h2>{name}</h2>
-//             <p>{desk}</p>
-//             <img src={foto} alt={name}/>
-//         </div>
-//     )
-// }
-//
-//
-//
-//
-//
-// export default Characters;
+function Characters(props) {
+    let {name, desk, foto} = props;
+
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>{desk}</p>
+            <img src={foto} alt={name}/>
+        </div>
+    )
+}
+
+
+
+
+
+export default Characters;
